207489efa70dc48f01718f1e83241f3feba75a6f
jaharaclark <jaharamclark@gmail.com>
2021-06-13 09:29:46 -1000

    Fix Test by parsing JSON

--- spec/request/api/v1/weather_spec.rb
+++ spec/request/api/v1/weather_spec.rb
@@ -6,21 +6,17 @@ RSpec.describe 'Weather', type: :request do
       - id, always set to null
       - type, always set to “forecast”
     attributes, an object containing weather information:' do
-    long_and_lat = MapquestFacade.get_location("denver,co")
-    returned_weather = ForecastFacade.weather_by_category(long_and_lat)
-
     get "/api/v1/forecast?",params:{location: "denver,co"}, headers:{"Content-Type": "application/json", "Accept": "application/json"}
     expect(response).to be_successful
+    parsed_response = JSON.parse(response.body, symbolize_names: true )
 
       
       # expect application json content type?
       # expect parse data class is a hash?
       # expect(response[:data][:id]).to eq("null" || nil)
-      expect(response[:data][:type]).to eq("forecast")
-      expect(response[:data][:attributes].first).to eq("current_weather")
-      expect(response[:data][:attributes].second).to eq("daily_weather")
-      expect(response[:data][:attributes].third).to eq("hourly_weather")
-      expect(response[:data][:attributes].keys.length).to eq(3)
+      expect(parsed_response[:data][:type]).to eq("forecast")
+      expect(parsed_response[:data][:attributes].keys).to eq([:current_weather, :hourly_weather, :daily_weather])
+      expect(parsed_response[:data][:attributes].keys.length).to eq(3)
       # take 16 - 18 and equate the keys to an array on one, two and three
       # take the keys for each attribute are the length they are supposed to be eg - current_weather.length = 10 - also put them in an array like comment on 20
   end
