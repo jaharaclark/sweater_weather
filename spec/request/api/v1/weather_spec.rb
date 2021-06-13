d620e327123eed404aae4d84ca434d8d19bd799d
jaharaclark <jaharamclark@gmail.com>
2021-06-13 10:03:15 -1000

    Complete Weather Test

--- spec/request/api/v1/weather_spec.rb
+++ spec/request/api/v1/weather_spec.rb
@@ -6,18 +6,19 @@ RSpec.describe 'Weather', type: :request do
       - id, always set to null
       - type, always set to “forecast”
     attributes, an object containing weather information:' do
+
     get "/api/v1/forecast?",params:{location: "denver,co"}, headers:{"Content-Type": "application/json", "Accept": "application/json"}
+   
     expect(response).to be_successful
     parsed_response = JSON.parse(response.body, symbolize_names: true )
 
-      
-      # expect application json content type?
-      # expect parse data class is a hash?
-      # expect(response[:data][:id]).to eq("null" || nil)
-      expect(parsed_response[:data][:type]).to eq("forecast")
-      expect(parsed_response[:data][:attributes].keys).to eq([:current_weather, :hourly_weather, :daily_weather])
-      expect(parsed_response[:data][:attributes].keys.length).to eq(3)
-      # take 16 - 18 and equate the keys to an array on one, two and three
-      # take the keys for each attribute are the length they are supposed to be eg - current_weather.length = 10 - also put them in an array like comment on 20
+    expect(parsed_response.class).to eq(Hash)
+    expect(parsed_response[:data][:id]).to eq(nil)
+    expect(parsed_response[:data][:type]).to eq("forecast")
+    expect(parsed_response[:data][:attributes].keys).to eq([:current_weather, :hourly_weather, :daily_weather])
+    expect(parsed_response[:data][:attributes].keys.length).to eq(3)
+    expect(parsed_response[:data][:attributes][:current_weather].keys).to eq([:datetime, :sunrise, :sunset, :temperature, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon])
+    expect(parsed_response[:data][:attributes][:daily_weather].first.keys).to eq([:date, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon])
+    expect(parsed_response[:data][:attributes][:hourly_weather].first.keys).to eq([:time, :temperature, :conditions, :icon])
   end
 end
