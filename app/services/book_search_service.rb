b9f7b42edf9ad9fb781f47b94e1437c813d182b9
jaharaclark <jaharamclark@gmail.com>
2021-06-14 06:01:22 -1000

    return info about location

--- app/services/book_search_service.rb
+++ app/services/book_search_service.rb
@@ -1,14 +1,19 @@
 class BookSearchService
   def self.get_requested_books(location_for_book, amount)
+    response = conn.get("search.json") do |request|
+      request.params['q'] = location_for_book
+      request.params['limit'] = amount
+    end
+    parse_response(response)
     require 'pry'; binding.pry
   end
 
   private
   def self.conn
-    Faraday.new("http://openlibrary.org/")
+    Faraday.new("http://openlibrary.org")
   end
 
   def self.parse_response(response)
-    JSON.parse_response(response.body, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
