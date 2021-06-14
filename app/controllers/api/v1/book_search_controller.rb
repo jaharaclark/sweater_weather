2bd16e99f63fb964849a61b75b201974ab548434
jaharaclark <jaharamclark@gmail.com>
2021-06-14 07:27:07 -1000

    fixed serializer

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
     book_list = BookSearchFacade.ask_for_book(params[:location], params[:quantity])
-    require 'pry'; binding.pry
+    render json: ReadingListSerializer(book_list)
   end
 end
--- app/facades/book_search_facade.rb
+++ app/facades/book_search_facade.rb
@@ -1,11 +1,10 @@
 class BookSearchFacade
   def self.ask_for_book(city_and_state, amount)
     returned_book_list = BookSearchService.get_requested_books(city_and_state, amount)
-    require 'pry'; binding.pry
     books_to_read = returned_book_list[:docs][0..5].map do |book|
       BookSearch.new(book)
     end
     count_of_returned_books = BookCount.new(returned_book_list)
+    ReadingList.new(books_to_read, count_of_returned_books)
   end
-  # ReadingList.new(books_to_read, count_of_returned_books)
 end
