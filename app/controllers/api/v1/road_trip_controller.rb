2b11131a3a4b6f9b2b44cb4347221a561631bd65
jaharaclark <jaharamclark@gmail.com>
2021-06-15 17:12:17 -1000

    restart section 4

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,16 +1,8 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    # if api_key == user.api_key
-      # do the things
-    # else
-      # say your credentials are invalid
-    # end
-    long_and_lat = MapquestFacade.get_location(params[:destination])
-    returned_weather = ForecastFacade.weather_by_category(long_and_lat)
-    travel_time = RoadTripFacade.make_a_road_trip(params[:origin], params[:destination])
-    
-    calculated_weather = WeatherCalculator.new(travel_time, returned_weather)
-    calculated_weather.figure_out_weather
+    user = User.find_by(api_key: params[:api_key])
+    require 'pry'; binding.pry
+    roadtrip = RoadTripFacade.get_info(params[:origin], params[:destination])
   end
 
 
